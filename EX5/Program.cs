f0ae7748305f992541c0aeb46887baf04fff34f7
MariaBezgodkova <ask.2004@yandex.ru>
2023-02-09 16:27:41 +0300

    add else

---
+++ EX5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Привет, как тебя зовут?");
+string username = Console.ReadLine ();
+
+if (username.ToLower () == "маша")
+{
+    Console.WriteLine ("Привет, дорогая!");
+}
+else
+{
+    Console.Write ("Добро пожаловать, ");
+    Console.WriteLine (username);
+}
