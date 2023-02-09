e3604f0c5fbf9f3f8a556612dddf06b69d192a4d
MariaBezgodkova <ask.2004@yandex.ru>
2023-02-09 16:07:12 +0300

    Add Random

--- EX4/Program.cs
+++ EX4/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 18;
-double numberB = 4;
-Console.WriteLine (numberA / numberB);
+﻿double numberA = new Random ().Next (1, 10); // 1,2,3...9
+Console.WriteLine (numberA);
+double numberB = new Random ().Next (1, 10);
+Console.WriteLine (numberB);
+Console.WriteLine (numberA + numberB);
