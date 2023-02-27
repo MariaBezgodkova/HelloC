54b532c8ee4e0b603059e219d43bd1f682064383
MariaBezgodkova <ask.2004@yandex.ru>
2023-02-27 12:20:25 +0300

    вычисление факториала с помощью рекурсии

---
+++ Factorial/Program.cs
@@ -0,0 +1,10 @@
+﻿// метод, который будет принимать число, факториал, которого надо вычислить
+double Factorial(int n) // тип данных указываем double - т.к. могут встретиться большие значения
+{
+    if (n == 1) return 1; // т.к. 1!=1
+    else return n * Factorial(n-1);
+}
+for (int i = 1;i<40; i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}");
+}
